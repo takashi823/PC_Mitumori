09091e1871564b16a1209a6e3af36e24980c3937
takashi823 <uchimura.takashi724@gmail.com>
2024-01-31 11:37:15 +0900

    fix: ルーティング追加

--- src/index.tsx
+++ src/index.tsx
@@ -11,7 +11,7 @@ const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement)
 root.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-      <BrowserRouter>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
         <App />
       </BrowserRouter>
     </ThemeProvider>
